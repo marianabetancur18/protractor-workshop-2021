6798aa26bb5c379a5f50054cb7c4b9a07d5949fa
marianabetancur18 <mbetancurf@unal.edu.com>
2021-10-26 17:02:38 -0500

    Point 16

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,8 +1,18 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+
+const { AwesomeReport } = require('jasmine-awesome-report');
+
+const config = {
+  fullPath: 'awesome-report',
+  fileName: 'report',
+  merge: true
+};
+
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: StacktraceOption.PRETTY
     }
   }));
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
 };
